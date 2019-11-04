49c2de92ae93436787918164de99585f18532c9d
Koji Arai <jca02266@gmail.com>
2019-11-04 19:53:11 +0900

    Fix configuration

--- src/main/java/org/jca02266/sonarplugins/errata/settings/ErrataProperties.java
+++ src/main/java/org/jca02266/sonarplugins/errata/settings/ErrataProperties.java
@@ -8,22 +8,23 @@ import org.sonar.api.config.PropertyDefinition;
 
 public class ErrataProperties {
 
-  public static final String CATEGORY = "Errata Properties Example";
+  public static final String CATEGORY = "Errata";
 
   private ErrataProperties() {
   }
 
   public static List<PropertyDefinition> getProperties() {
     return asList(
-      PropertyDefinition.builder("errata.text")
-        .name("Errata１")
-        .description("Errata properties 1")
-        .defaultValue("タイポ")
+      PropertyDefinition.builder("errata.file.suffixes")
+        .name("File suffixes")
+        .description("Comma-separated list of suffixes for files to analyze")
+        .multiValues(true)
+        .defaultValue(".java")
         .category(CATEGORY)
         .build(),
-      PropertyDefinition.builder("errata.text2")
-        .name("Errata２")
-        .description("Errata properties 2")
+      PropertyDefinition.builder("errata.table")
+        .name("Errata list")
+        .description("Errata which format is \"wrong word regexp -> right word\"")
         .multiValues(true)
         .category(CATEGORY)
         .build());
