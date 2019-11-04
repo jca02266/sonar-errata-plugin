3bad1bd6dba40bfb1d7a9ea44d7fdaed41e3983f
Koji Arai <jca02266@gmail.com>
2019-11-04 14:10:18 +0900

    Add logger and config

--- src/main/java/org/jca02266/sonarplugins/errata/rules/CreateIssuesOnTextFilesSensor.java
+++ src/main/java/org/jca02266/sonarplugins/errata/rules/CreateIssuesOnTextFilesSensor.java
@@ -3,6 +3,7 @@ package org.jca02266.sonarplugins.errata.rules;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.util.Arrays;
 import java.util.regex.Pattern;
 
 import org.sonar.api.batch.fs.FileSystem;
@@ -12,13 +13,24 @@ import org.sonar.api.batch.sensor.SensorContext;
 import org.sonar.api.batch.sensor.SensorDescriptor;
 import org.sonar.api.batch.sensor.issue.NewIssue;
 import org.sonar.api.batch.sensor.issue.NewIssueLocation;
+import org.sonar.api.config.Configuration;
+import org.sonar.api.utils.log.Logger;
+import org.sonar.api.utils.log.Loggers;
 
 public class CreateIssuesOnTextFilesSensor implements Sensor {
+  private static final Logger LOGGER = Loggers.get(CreateIssuesOnTextFilesSensor.class);
 
   private static final double ARBITRARY_GAP = 1.0;
 
+  protected final Configuration config;
+
+  public CreateIssuesOnTextFilesSensor(final Configuration config) {
+    LOGGER.info("errata sensor constructor called");
+    this.config = config;
+  }
   @Override
   public void describe(SensorDescriptor descriptor) {
+    LOGGER.info("errata sensor describe() called");
     descriptor.name("Check errata for text files");
 
     descriptor.onlyOnLanguage("java");
@@ -27,6 +39,11 @@ public class CreateIssuesOnTextFilesSensor implements Sensor {
 
   @Override
   public void execute(SensorContext context) {
+    LOGGER.info("errata sensor execute() called");
+
+    LOGGER.info("errata text: {}", config.get("errata.text").orElse("default"));
+    Arrays.stream(config.getStringArray("errata.text2")).forEach(val -> LOGGER.info("errata text2: {}", val));
+
     FileSystem fs = context.fileSystem();
     Iterable<InputFile> textFiles = fs.inputFiles(fs.predicates().all());
     for (InputFile textFile : textFiles) {
